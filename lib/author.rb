e4f912ad9168ee03e3f91b534ce62094508c7bef
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-19 22:18:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,4 +5,13 @@ class Author
     @name = name
     @posts = []
   end
+
+  def add_post(post)
+    post.author = self 
+  end 
+
+  def add_post_by_title(title)
+    new_title = Post.new(title)
+    add_post(new_title)
+  end
 end
