f551e68a2c08488904a534fccf34a514f06866bd
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-19 22:13:19 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,17 @@
 class Post
   attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    save 
+  end 
+
+  def save 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
 end
